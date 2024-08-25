3c68ffac5bc286801ab8302703795beaa43e8c59
AnnekkeH <112107957+AnnekkeH@users.noreply.github.com>
2024-08-25 15:50:26 -0600

    End of Moment Update
    Just updating github to match because I'm bored. Removed Style.css and put it inline in hello.html. 
    Need to actually learn DOM now and start actually changing google drive

--- content.js
+++ content.js
@@ -1,10 +1,8 @@
-// chromes example 
-
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) { // no clue how this stuff works
-  if (tabs[0].url === "https://drive.google.com/drive/my-drive"){
-    console.log("god should fear me");
+  if (tabs[0].url.includes("https://drive.google.com/drive/")){  // if the URL is a google drive page
+    console.log("god should fear me"); // print // TODO: this is where the actual actions go
+  } else {
+    console.log("god has won"); // print
   }
-});
-
-
+}); // this function should probably go into background or the service worker cuz of what it does
 
